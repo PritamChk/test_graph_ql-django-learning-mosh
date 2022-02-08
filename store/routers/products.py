caea4defe49376b88d28efc24429ba38e6c5fcfa
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-08 20:46:24 +0530

    ordered products link done

--- store/routers/products.py
+++ store/routers/products.py
@@ -1,8 +1,7 @@
 from ninja import Router
-
+from django.db.models import F,Q
 from store.schema import ProductOut
-
-from ..models import Product
+from ..models import Product,Order,OrderItem
 
 
 product_router = Router(tags=["Product"])
@@ -11,4 +10,9 @@ product_router = Router(tags=["Product"])
 def hello_prod(request,offset:int=0,limit:int=10):
     qs = Product.objects.filter(promotions__isnull = False)
     
+    return [ p for p in qs[offset:offset+limit] ]
+
+@product_router.get("/ordered",response=list[ProductOut])
+def get_ordered_prod(request,offset:int=0,limit:int=10):
+    qs = Product.objects.filter(id__in = OrderItem.objects.values("product_id").distinct())
     return [ p for p in qs[offset:offset+limit] ]
