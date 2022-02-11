42707ae75e388b9c49993326f738ae64a53fbde8
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-11 15:09:26 +0530

    jani na eta ki korechi

--- store/schema_graphene.py
+++ store/schema_graphene.py
@@ -3,25 +3,54 @@ import graphene as gfn
 from graphene_django import DjangoObjectType as DOT
 from store.models import *
 
+
+class CustomerType(DOT):
+    class Meta:
+        model =Customer
+        fields = ("first_name","email","membership")
+
+
 class ProductType(DOT):
     class Meta:
         model = Product
-        fields = ("id","title","price","description","inventory","last_update")
+        fields = ("title","price","promotions","collection")
 
 class PromotionType(DOT):
     class Meta:
         model = Promotion
-        fields = ("discount","description")        
-
-class ProductWithPromotionType(DOT):
+        fields = ("discount","description")   
+             
+class CollectionType(DOT):
     class Meta:
-        model = Product
-        exclude = ("collection",)
+        model = Collection
+        fields = ("id","title")        
+
+class OrderType(DOT):
+    class Meta:
+        model = Order
+        fields = ("payment_status","customer")
+        
+class OrderItemType(DOT):
+    class Meta:
+        model = OrderItem
+        fields = ("order","product","quantity","unit_cost")
+
 
 class Query(gfn.ObjectType):
     get_products = gfn.List(ProductType)
-    products_with_promotion = gfn.List(ProductWithPromotionType)
     get_promo = gfn.List(PromotionType)
+    get_collection = gfn.List(CollectionType)
+    get_customer = gfn.List(CustomerType)
+    get_customer_bought_products = gfn.List(CustomerType)
+    # def resolve_get_collection(root,info):
+    #     return Collection.objects.all()
+    # 
+    
+    # def resolve_get_customer_bought_products(root,info):
+    #     return OrderItem.objects.all().filter()
+    
+    def resolve_get_customer(root,info):
+        return Customer.objects.all()
     
     def resolve_get_promo(root,info):
         return Promotion.objects.all()
@@ -29,8 +58,8 @@ class Query(gfn.ObjectType):
     def resolve_get_products(root,info):
         return Product.objects.all().order_by("id")
     
-    def resolve_products_with_promotion(root,info):
-        qs = Product.objects.filter(promotions__isnull=False)
-        return qs
+    # def resolve_products_with_promotion(root,info):
+    #     qs = Product.objects.filter(promotions__isnull=False)
+    #     return qs
 
 schema = gfn.Schema(query=Query)
